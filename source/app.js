f83b1fe31d1ba56f8e07d70835d7bf571a5708c4
vishvajeet-y <vishvaj_bt18@iiitkalyani.ac.in>
2020-03-13 02:37:29 +0530

    Remove unnecessary console.log

--- source/app.js
+++ source/app.js
@@ -4,8 +4,6 @@ const hbs=require('hbs')
 const geocode1=require('./utils/geocode')
 const forecast1=require('./utils/forecast')
 const chalk=require('chalk')
-//console.log(__dirname)
-//console.log(path.join(__dirname,'../public'))
 const app=express()
 //Define path for Express config
 const public_directoy=path.join(__dirname,'../public')
@@ -83,9 +81,7 @@ app.use(express.static(public_directoy))
             })
         }
         
-     //   This is upto lecture 8.3
-       // res.send({location:'West Bengal',weather:'cloudy',address:req.query.address})
-      // res.send([{location:'West Bengal',weather:'cloudy',address},{location:'UttarPradesh',weather:'sunny'}]) sending array of object
+    
     })
     app.get('/product',(req,res)=>
     {
@@ -121,25 +117,3 @@ app.listen(3000,()=>
 {
     console.log('server is up on port 3000')
 })
-//This is upto lecture 7.3 wher we are trying to parse json and html file in our code
-/*
-app.get('',(req,res)=>
-    {
-          res.send('<h1>HOME</h1>')
-    })
-    app.get('/help',(req,res)=>
-    {
-          res.send([{
-              name:'Vishvajeet',
-              age:21
-          },
-          {
-              name:'Tipu',
-              age:20
-          }
-        ])
-    })
-    app.get('/about',(req,res)=>
-    {
-        res.send('<h1>About</h1>')
-    })*/
